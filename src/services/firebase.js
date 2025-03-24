e3324d956176f79e83474a61f5f9a96c71c708b9
Aniket25042003 <aniketg2003patel@gmail.com>
2025-03-24 17:05:53 -0400

    second commit

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,15 +2,24 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
+import { 
+  FIREBASE_API_KEY, 
+  FIREBASE_AUTH_DOMAIN, 
+  FIREBASE_PROJECT_ID, 
+  FIREBASE_STORAGE_BUCKET, 
+  FIREBASE_MESSAGING_SENDER_ID, 
+  FIREBASE_APP_ID, 
+  FIREBASE_MEASUREMENT_ID 
+} from '@env';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCW1JgFzLRm5cShljiCvU1RNPgTM8vuxB4",
-  authDomain: "meetingassistant-4f53e.firebaseapp.com",
-  projectId: "meetingassistant-4f53e",
-  storageBucket: "meetingassistant-4f53e.firebasestorage.app",
-  messagingSenderId: "861055197412",
-  appId: "1:861055197412:web:2f24bf8a0acbdb760a0cf1",
-  measurementId: "G-XJC4594X3W"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 if (!firebase.apps.length) {
